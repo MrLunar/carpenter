0d28cf3499f589346433273e774c397e09b748c2
Mark Bowker <MrLunar@users.noreply.github.com>
2018-02-23 17:34:29 +0000

    win2016: increase vagrant mem

--- windows-2016/resources/vagrant/vagrantfile.rb
+++ windows-2016/resources/vagrant/vagrantfile.rb
@@ -5,6 +5,6 @@ Vagrant.configure('2') do |config|
   config.vm.network :forwarded_port, guest: 3389, host: 33389, id: 'rdp', auto_correct: true
 
   config.vm.provider 'virtualbox' do |v|
-    v.memory = 1024
+    v.memory = 2048
   end
 end
