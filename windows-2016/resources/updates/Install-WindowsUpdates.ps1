6d14d43b58a06c4baa0f4bfcd6e03ef8c2d4b227
Mark Bowker <MrLunar@users.noreply.github.com>
2018-02-25 11:28:37 +0000

    win2016: tweaks to update process
     - Don't set categories for updates as previous categories don't include CUs.
     - Tweak workaround for latest CUs not being available.

--- windows-2016/resources/updates/Install-WindowsUpdates.ps1
+++ windows-2016/resources/updates/Install-WindowsUpdates.ps1
@@ -1,38 +1,5 @@
 $ErrorActionPreference = "Stop"
-
-# Write-Host "Installing pre-requisites ..."
-# Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force
-# Install-Module PSWindowsUpdate -RequiredVersion 2.0.0.3 -Force 
-
-# Write-Host "Performing initial search for Windows Updates..."
-# $updates = Get-WindowsUpdate -Verbose
-# $updates | Format-List -Property KB,Title,Size
-
-# This is a hack fix for new installations not being able to retrieve
-# the latest cumulative update (2018-01 at time of writing).
-# It is unknown if this is a specific issue with this CU or something else.
-# The only workaround found so far is to allow the system to "settle", which
-# somehow allows the system to be provided the latest updates.
-# Unfortunately this "settling" takes a long time (15-30mins)
-# Fingers crossed this is only a temporary issue...
-# if ($updates.KB -like 'KB4053579') {
-#     Write-Host "Sleeping for 30m due to a clean install not being able to retrieve the latest updates..."
-#     Start-Sleep -s 1800
-    
-#     Write-Host "Performing another search for Windows Updates..."
-#     $updates = Get-WindowsUpdate -Verbose
-#     $updates | Format-List -Property KB,Title,Size
-# }
-
-# if ($updates.KB -like 'KB4053579') {
-#     Write-Host "ERROR: WU still retrieving old updates"
-#     Write-Host "ERROR: Increase timeout or find another workaround."
-#     exit 1
-# }
-
-# Write-Host "Installing Windows Updates..."
-# Install-WindowsUpdate -AcceptAll -IgnoreReboot -Verbose `
-#     | Format-List -Property X,Result,KB,Size,Title
+Set-StrictMode -Version 2
 
 Write-Host "Installing pre-requisites..."
 Add-Type -AssemblyName System.IO.Compression.Filesystem
@@ -44,13 +11,34 @@ if (!(Test-Path "$installPath\pswindowsupdate")) {
 }
 Import-Module PSWindowsUpdate
 
-# Ignoring KB4053579 is a hackfix for new Win2016 installs.
-# It requires 15-30 minutes of uptime before Windows Update is able to retrieve
-# the real latest cumulative update.
-Write-Host "Installing updates..."
-Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput `
-    -Category "Critical Update","Security Update","Definition Update","Update Rollup" `
-    -NotKBArticleID "KB4053579"
+Write-Host "Checking for Windows updates..."
+$updates = Get-WUInstall -ListOnly -AcceptAll -IgnoreReboot -IgnoreUserInput
+
+if ($updates) {
+    # This is a hack fix for new installations sometimes not being able to 
+    # retrieve the latest cumulative update.
+    # It is unknown if this is a specific issue with this CU or something else.
+    # The only workaround found so far is to allow the system to "settle", which
+    # somehow allows the system to be provided the latest updates.
+    # Unfortunately this "settling" takes a long time (15-30mins)
+    # Fingers crossed this is only a temporary issue...
+    if ($updates.KB -like 'KB4053579') {
+        Write-Host "Sleeping for 10m due to a clean install not being able to retrieve the latest updates sometimes..."
+        Start-Sleep -s 600
+        
+        Write-Host "Performing another search for Windows Updates..."
+        $updates = Get-WUInstall -ListOnly -AcceptAll -IgnoreReboot -IgnoreUserInput
+    }
+    
+    if ($updates.KB -like 'KB4053579') {
+        Write-Host "ERROR: WU still retrieving old updates"
+        Write-Host "ERROR: Increase timeout or find another workaround."
+        exit 1
+    }
+    
+    Write-Host "Installing Windows updates..."
+    Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput
+}
 
 Write-Host "Cleaning up..."
 Remove-Module PSWindowsUpdate
