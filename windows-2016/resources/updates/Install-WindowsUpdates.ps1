2670453d81eeda2c00d11e1c72c316ab9d4389e1
Mark Bowker <MrLunar@users.noreply.github.com>
2018-03-03 14:44:09 +0000

    win2016: updates finally fixed!
    Because Windows Defender hadn't updated yet and set the required registry key stimpulated by Microsoft, windows updates was ignoring the latest updates.
    
    See: https://support.microsoft.com/en-gb/help/4072699/january-3-2018-windows-security-updates-and-antivirus-software

--- windows-2016/resources/updates/Install-WindowsUpdates.ps1
+++ windows-2016/resources/updates/Install-WindowsUpdates.ps1
@@ -11,37 +11,21 @@ if (!(Test-Path "$installPath\pswindowsupdate")) {
 }
 Import-Module PSWindowsUpdate
 
-Write-Host "Checking for Windows updates..."
-$updates = Get-WUInstall -ListOnly -AcceptAll -IgnoreReboot -IgnoreUserInput
-
-if ($updates) {
-    # This is a hack fix for new installations sometimes not being able to 
-    # retrieve the latest cumulative update.
-    # It is unknown if this is a specific issue with this CU or something else.
-    # The only workaround found so far is to allow the system to "settle", which
-    # somehow allows the system to be provided the latest updates.
-    # Unfortunately this "settling" takes a long time (15-30mins)
-    # Fingers crossed this is only a temporary issue...
-    if ($updates.KB -like 'KB4053579') {
-        Write-Host "Sleeping for 10m due to a clean install not being able to retrieve the latest updates sometimes..."
-        Start-Sleep -s 600
-        
-        Write-Host "Performing another search for Windows Updates..."
-        $updates = Get-WUInstall -ListOnly -AcceptAll -IgnoreReboot -IgnoreUserInput
-    }
-    
-    if ($updates.KB -like 'KB4053579') {
-        Write-Host "ERROR: WU still retrieving old updates"
-        Write-Host "ERROR: Increase timeout or find another workaround."
-        exit 1
-    }
-    
-    Write-Host "Installing Windows updates..."
-    Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput
+$qualityCompatKeyAdded = $false
+if (!(Test-Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat")) {
+    New-Item "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat"
+    New-ItemProperty "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat" -Name "cadca5fe-87d3-4b96-b7fb-a231484277cc" -PropertyType dword -Value 0
+    $qualityCompatKeyAdded = $true
 }
+
+Write-Host "Installing Windows updates..."
+Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput
 
 Write-Host "Cleaning up..."
 Remove-Module PSWindowsUpdate
 Remove-Item "c:\windows\system32\windowspowershell\v1.0\modules\pswindowsupdate" -Recurse
+if ($qualityCompatKeyAdded) {
+    Remove-Item -Recurse "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat"
+}
 
 Write-Host "Completed installing Windows updates."
