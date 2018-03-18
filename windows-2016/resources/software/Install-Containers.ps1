038c7e63dc69a1db1614d63a237018d4f0f277b2
Mark Bowker <MrLunar@users.noreply.github.com>
2018-03-18 10:44:36 +0000

    win2016: change method of installing docker
    Install manually due to reliability issues with the package providers

--- windows-2016/resources/software/Install-Containers.ps1
+++ windows-2016/resources/software/Install-Containers.ps1
@@ -4,13 +4,28 @@ Set-StrictMode -Version 2.0
 Write-Host "Installing containers feature..."
 Install-WindowsFeature Containers
 
-Write-Host "Installing nuget package provider..."
-Install-PackageProvider -Name NuGet -RequiredVersion "2.8.5.201" -Force
+# Due to a multitude of issues using the package providers unattended, we'll install manually instead
+# https://docs.docker.com/install/windows/docker-ee/#use-a-script-to-install-docker-ee
 
-Write-Host "Installing docker package provider..."
-Install-Module -Name DockerMsftProvider -Repository PSGallery -RequiredVersion "1.0.0.4" -Force
+Write-Host "Downloading docker..."
+$url = "https://download.docker.com/components/engine/windows-server/17.06/docker-17.06.2-ee-6.zip"
+$hash = "FC31D16C3EFD4E3769A58639C207410D840E89C6B4E36BF78B8FC564FE06CAA5"
+$tempDir = "C:\Windows\Temp"
+(New-Object Net.WebClient).DownloadFile($url, "$tempDir\docker.zip")
+$downloadHash = (Get-FileHash -Algorithm SHA256 "$tempDir\docker.zip").Hash
+if ($downloadHash -ne $hash) {
+    Write-Output "ERROR: Download checksum does not match."
+    Write-Output "ERROR:   Was: $downloadHash"
+    Write-Output "ERROR:   Expected: $hash"
+    exit 1
+}
 
 Write-Host "Installing docker..."
-Install-Package -Name docker -ProviderName DockerMsftProvider -RequiredVersion "17.06.2-ee-6" -Force
+Expand-Archive "$tempDir\docker.zip" -DestinationPath $Env:ProgramFiles
+Remove-Item "$tempDir\docker.zip"
+$env:path += ";$env:ProgramFiles\docker"
+$newPath = "$env:ProgramFiles\docker;" + [Environment]::GetEnvironmentVariable("PATH", "Machine")
+[Environment]::SetEnvironmentVariable("PATH", $newPath, "Machine")
+Start-Process "dockerd.exe" "--register-service" -Wait
 
 Write-Host "Containers setup completed successfully."
